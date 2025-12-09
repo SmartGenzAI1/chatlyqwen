a2c844eaec0585b8ab315cb1ab50456cc51b5a89
Saff9 <saffanakbar942@gmail.com>
2025-12-09 13:15:38 +0530

    Create MainActivity.java

---
+++ android/app/src/main/java/com/chatly/app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.chatly.app;
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterFragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
