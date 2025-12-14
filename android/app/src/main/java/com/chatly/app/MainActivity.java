55e7fd7088d5ecee104d5c9c89fc82fb76458e64
Saff9 <saffanakbar942@gmail.com>
2025-12-14 18:36:55 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/chatly/app/MainActivity.java
+++ android/app/src/main/java/com/chatly/app/MainActivity.java
@@ -1,6 +1,19 @@
 package com.chatly.app;
 
-import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
