2ea861b7c5d8c795a477adac4893ecd903b55a1b
Saff9 <saffanakbar942@gmail.com>
2025-12-14 17:32:35 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/chatly/app/MainActivity.java
+++ android/app/src/main/java/com/chatly/app/MainActivity.java
@@ -1,6 +1,6 @@
 package com.chatly.app;
 
-import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterFragmentActivity {
+public class MainActivity extends FlutterActivity {
 }
